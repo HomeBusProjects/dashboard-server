8f5c09e490a2b1690a7f5a929ab154f8c02ac72b
John Romkey <58883+romkey@users.noreply.github.com>
2019-12-26 20:48:17 -0800

    properly save new Slack user before continuing
    get rid of some debugging info

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,17 +2,13 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def slack
     @user = User.from_omniauth_slack(request.env["omniauth.auth"])
 
-#    if @user.persisted?
-    if true
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Slack") if is_navigational_format?
-    else
-      session["devise.slack_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+    set_flash_message(:notice, :success, kind: "Slack") if is_navigational_format?
   end
 
   def failure
+    pp '>>>>>>>>>>>>. CALLBACKSCONTROLLER.SLACK FAILURE'
+
     redirect_to '/'
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[slack]
 
   def self.from_omniauth_slack(auth)
-    pp auth
-    
     where(slack_uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
-
-      pp auth
+      user.save
 
       return user
     end
