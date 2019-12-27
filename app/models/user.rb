2595a3a988b8213702d1aebf8a95721f579499e5
John Romkey <58883+romkey@users.noreply.github.com>
2019-12-26 21:06:41 -0800

    make initial user admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
+      user.admin = (User.count == 0)
       user.save
 
       return user
